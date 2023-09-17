52967dfa665eadde15bf39dcc6a2a454b439f170
rbtessier <r.b.tessier@gmail.com>
2023-09-16 21:40:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
+#trying to create a custom slider color but it doesnn't like me
 def custom_slider_style(color="#be9e44"):  # Use your desired color
     custom_css = f"""
     <style>
@@ -22,6 +23,7 @@ def custom_slider_style(color="#be9e44"):  # Use your desired color
 
 def display_feature_importance(model, X):
 
+    #Later I'll make this more global as this is a useful mapping for better code but for now its heara
     description_dict = {
     'GrLivArea': 'Above ground living area square feet.',  
     'GarageCars': 'Size of garage in car capacity.',
@@ -30,12 +32,15 @@ def display_feature_importance(model, X):
     'FullBath': 'Number of full bathrooms.',
     'YearBuilt': 'Original construction date.'
     }
+    
     # Get feature importances-
     importances = model.feature_importances_
+
+    # Create a description column
     description = [description_dict.get(feature, '') for feature in X.columns]
+    
     # Associate importances with feature names
     feature_importance = pd.DataFrame({'Feature': X.columns, 'Importance': importances, 'Description' : description})
-    
     
     # Sort features by importance in descending order
     feature_importance = feature_importance.sort_values('Importance', ascending=False)
@@ -66,7 +71,7 @@ This app predicts the price of a house in **Ames, Iowa** based on specific featu
 After selecting the following features, click the **'Predict'** button at the bottom of the page to see the predicted house price.
 """)
 
-custom_slider_style("#be9e44") 
+
 #overall_qual = st.slider('Overall Quality', min_value=1, max_value=10, value=5)
 grliv_area = st.slider('Above Ground Living Area (sq. ft.)', min_value=0, max_value=10000, value=2000)
 garage_cars = st.slider('Garage Size (Cars)', min_value=0, max_value=4, value=2)
@@ -74,7 +79,7 @@ total_bsmt_sf = st.slider('Total Basement Area (sq. ft.)', min_value=0, max_valu
 bedroom = st.slider('Number of Bedrooms', min_value=0, max_value=8, value=2)
 full_bath = st.slider('Number of Full Bathrooms', min_value=0, max_value=4, value=2)
 year_built = st.slider('Year the House was Built', min_value=1900, max_value=2023, value=2000)
-
+custom_slider_style("#be9e44") 
 
 if st.button('Predict'):
     #input_data_w_features = np.array([[overall_qual, grliv_area, garage_cars, total_bsmt_sf,bedroom, full_bath, year_built]])
